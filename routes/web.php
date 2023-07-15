e006446ca4b9e2ff77350cd420ba5ec6d93a235d
wayneOSII <skybe900@gmail.com>
2023-07-15 14:18:30 +0800

    20230715-1

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('/');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return redirect('/admin');
     })->name('dashboard');
 });
+
+Route::get('/admin/login', function () {
+    return redirect()->route('/');
+})->name('filament.auth.login');
