1fb70b9ba5d114facec2191a4edd32a467b5ab01
wayneOSII <skybe900@gmail.com>
2023-07-16 14:54:37 +0800

    20230716-2

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\RolesRelationManager;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Pages\Page;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+use phpDocumentor\Reflection\Types\Null_;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $navigationGroup = 'Admin Management';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_admin')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('student_id')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->maxLength(255)
+                    ->dehydrateStateUsing(static fn (null|string $state): 
+                        null|string => 
+                        filled($state) ? Hash::make($state): null
+                        )
+                    ->required(static fn (Page $livewire): string => $livewire instanceof CreateUser,)
+                    ->dehydrated(static fn (null|string $state): bool => filled($state),)
+                    ->label(static fn (Page $livewire): string => 
+                        ($livewire instanceof EditUser) ? 'New Password' : 'Password'),
+                CheckboxList::make('roles')
+                    ->relationship('roles','name')
+                    ->columns(2)
+                    ->helperText('Only Choose One!')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\IconColumn::make('is_admin')
+                    ->boolean()
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('roles.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('student_id'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime('d-M-Y')
+                    ->sortable()
+                    ->searchable(),
+                // Tables\Columns\TextColumn::make('updated_at')
+                //     ->dateTime(),
+                TextColumn::make('deleted_at')
+                    ->dateTime('d-M-Y')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            RolesRelationManager::class,
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }    
+}
